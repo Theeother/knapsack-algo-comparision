784baadb38c144f51ee04831b03cc95a0c67e847
Ahmed Khalil Seddik <ka.seddik@gmail.com>
2022-12-31 16:43:05 +0100

    Plots working

--- Tests/main.py
+++ Tests/main.py
@@ -4,59 +4,79 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-# setting path 
-sys.path.insert(0, 'C:/Users/aksed/OneDrive/Bureau/softComputing/Single_solution_algorithms')
+# setting path
 
 # import ../Single_solution_algorithms/simulated_annealing.py
-import simulated_annealing 
-import glouton_search
-import tabu_search 
+
+
+def importing():
+    sys.path.insert(
+        0, 'C:/Users/aksed/OneDrive/Bureau/softComputing/Single_solution_algorithms')
+    import simulated_annealing
+    import glouton_search
+    import tabu_search
+    return simulated_annealing, glouton_search, tabu_search
+
+
+def importing_plots():
+    sys.path.insert(
+        0, 'C:/Users/aksed/OneDrive/Bureau/softComputing/Tests/plots')
+    import ploting
+    return ploting
+
+
+simulated_annealing, glouton_search, tabu_search = importing()
+
 
 class Team1:
     def __init__(self):
         self.name = "Team 1"
-        self.solvingMethods = ["simulated_annealing","glouton_search","tabu_search"] # List of methods that are exactly the name of the methods used
+        # List of methods that are exactly the name of the methods used
+        self.solvingMethods = ["simulated_annealing",
+                               "glouton_search", "tabu_search"]
 
-    def simulated_annealing(self, numberOfElements, listOfElements , backPackSize):
-        return simulated_annealing.simulated_annealing_method(numberOfElements, listOfElements , backPackSize)
+    def simulated_annealing(self, numberOfElements, listOfElements, backPackSize):
+        return simulated_annealing.simulated_annealing_method(numberOfElements, listOfElements, backPackSize)
 
-    def glouton_search(self, numberOfElements, listOfElements , backPackSize):
+    def glouton_search(self, numberOfElements, listOfElements, backPackSize):
 
-        return glouton_search.glouton_search_method(numberOfElements, listOfElements , backPackSize)
+        return glouton_search.glouton_search_method(numberOfElements, listOfElements, backPackSize)
 
-    def tabu_search(self, numberOfElements, listOfElements , backPackSize):
+    def tabu_search(self, numberOfElements, listOfElements, backPackSize):
         if numberOfElements > 10:
-              return [0, '0'*numberOfElements]
-        return tabu_search.tabu_search_method(numberOfElements, listOfElements , backPackSize)
-    
+            return [0, '0'*numberOfElements]
+        return tabu_search.tabu_search_method(numberOfElements, listOfElements, backPackSize)
+
     # Don't forget to add any new methods name to the solvingMethods list above
     # Edit only team 1, and the program will automatically compare the results of both teams and all the methods used by each team
+
 
 class Team2:
     def __init__(self):
         self.name = "Team 2"
-        self.solvingMethods = ["bruteForce"] 
-    
-    def bruteForce(self, numberOfElements, listOfElements , backPackSize):
-      # brute force knapsack 0/1
-      if numberOfElements >10:
-        return [0, '0'*numberOfElements]
-      maxi = 0
-      answer = ''
-      for i in range(2**numberOfElements):
-        weight = 0
-        value = 0
-        for j in range(numberOfElements):
-          if i & (1 << j):
-            weight += listOfElements[j][1]
-            value += listOfElements[j][0]
-        if weight <= backPackSize:
-          if value > maxi:
-            maxi = value
-            answer = i 
-      answer = bin(answer)[2:].zfill(numberOfElements)[::-1]
+        self.solvingMethods = ["bruteForce"]
 
-      return [maxi, answer]
+    def bruteForce(self, numberOfElements, listOfElements, backPackSize):
+        # brute force knapsack 0/1
+        if numberOfElements > 10:
+            return [0, '0'*numberOfElements]
+        maxi = 0
+        answer = ''
+        for i in range(2**numberOfElements):
+            weight = 0
+            value = 0
+            for j in range(numberOfElements):
+                if i & (1 << j):
+                    weight += listOfElements[j][1]
+                    value += listOfElements[j][0]
+            if weight <= backPackSize:
+                if value > maxi:
+                    maxi = value
+                    answer = i
+        answer = bin(answer)[2:].zfill(numberOfElements)[::-1]
+
+        return [maxi, answer]
+
 
 def check_solution_validity(solution, item_list, backPackSize):
     value = 0
@@ -65,32 +85,34 @@ def check_solution_validity(solution, item_list, backPackSize):
         if solution[1][i] == '1':
             value += item_list[i][0]
             weight += item_list[i][1]
-    return(round(solution[0],6)==round(value,6) and weight<=backPackSize)
+    return (round(solution[0], 6) == round(value, 6) and weight <= backPackSize)
 
 
 def run_method(method, numberOfElements, listOfElements, backPackSize, optimalSolution):
     # This function will run the method and return the time it took to run 100 times, the solution accuracy
     # and the solution itself
 
-    # Run the method 100 times 
-    time = timeit.timeit(lambda: method(numberOfElements, listOfElements, backPackSize), number=1)*100
+    # Run the method 100 times
+    time = timeit.timeit(lambda: method(
+        numberOfElements, listOfElements, backPackSize), number=1)*100
     # Get the solution
     solution = method(numberOfElements, listOfElements, backPackSize)
     # Check if the solution is valid
-    solutionValidity = check_solution_validity(solution, listOfElements, backPackSize)
+    solutionValidity = check_solution_validity(
+        solution, listOfElements, backPackSize)
     # Check solution accuracy
     # if solutionValidity:print("---------- this is valid ",solution[0], optimalSolution,method)
     solutionAccuracy = 0
     if not solutionValidity:
         print(method)
-        print(optimalSolution,solution,solutionValidity)
+        print(optimalSolution, solution, solutionValidity)
     # print(solution[0], optimalSolution)
     if solutionValidity:
-        solutionAccuracy = 100*(solution[0])/optimalSolution 
-    
-    return {"time":time, "solutionValidity":solutionValidity, "solutionAccuracy":solutionAccuracy, "solution":solution}
+        solutionAccuracy = 100*(solution[0])/optimalSolution
 
-    
+    return {"time": time, "solutionValidity": solutionValidity, "solutionAccuracy": solutionAccuracy, "solution": solution}
+
+
 # data structure :
 # data = {low-dim:
 #   team1:
@@ -101,7 +123,7 @@ def run_method(method, numberOfElements, listOfElements, backPackSize, optimalSo
 #   ...}
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     # list of Teams
     teams = [Team1(), Team2()]
     list_of_scales = ["low_scale", "large_scale"]
@@ -116,22 +138,29 @@ if __name__ == "__main__" :
                 data[scale][team.name][method]["time_list"] = []
                 data[scale][team.name][method]["accuracy_list"] = []
             # list of test cases for each scale
-                list_of_test_cases = os.listdir("test_data/"+scale)
+                print(os.getcwd())
+                list_of_test_cases = os.listdir("Tests/test_data/"+scale)
                 for test_case in list_of_test_cases:
                     # get the optimal solution
-                    optimalSolution = float(open("test_data/"+scale+"_optimum/"+test_case).read())
+                    optimalSolution = float(
+                        open("Tests/test_data/"+scale+"_optimum/"+test_case).read())
                     # get the test case data
-                    with open("test_data/"+scale+"/"+test_case) as f:
-                        numberOfElements, backPackSize = map(int, f.readline().split())
+                    with open("Tests/test_data/"+scale+"/"+test_case) as f:
+                        numberOfElements, backPackSize = map(
+                            int, f.readline().split())
                         listOfElements = []
                         for i in range(numberOfElements):
-                            listOfElements.append(list(map(float, f.readline().split())))
+                            listOfElements.append(
+                                list(map(float, f.readline().split())))
                     # run the methods
 
-                    result = run_method(getattr(team, method), numberOfElements, listOfElements, backPackSize, optimalSolution)
-                    data[scale][team.name][method]["nb_valid"]+=result["solutionValidity"]
-                    data[scale][team.name][method]["time_list"].append(result["time"])
-                    data[scale][team.name][method]["accuracy_list"].append(result["solutionAccuracy"])
+                    result = run_method(getattr(
+                        team, method), numberOfElements, listOfElements, backPackSize, optimalSolution)
+                    data[scale][team.name][method]["nb_valid"] += result["solutionValidity"]
+                    data[scale][team.name][method]["time_list"].append(
+                        result["time"])
+                    data[scale][team.name][method]["accuracy_list"].append(
+                        result["solutionAccuracy"])
 
     # print the results
     for scale in list_of_scales:
@@ -140,13 +169,16 @@ if __name__ == "__main__" :
             print("Team : ", team.name)
             for method in team.solvingMethods:
                 print("Method : ", method)
-                print("Number of valid solutions : ", data[scale][team.name][method]["nb_valid"])
-                print("Average time : ", np.mean(data[scale][team.name][method]["time_list"]))
-                print("Average accuracy : ", np.mean(data[scale][team.name][method]["accuracy_list"]))
+                print("Number of valid solutions : ",
+                      data[scale][team.name][method]["nb_valid"])
+                print("Average time : ", np.mean(
+                    data[scale][team.name][method]["time_list"]))
+                print("Average accuracy : ", np.mean(
+                    data[scale][team.name][method]["accuracy_list"]))
                 print("")
             print("")
         print("")
-        
+
     print(data)
 
     # plot the results
@@ -160,15 +192,7 @@ if __name__ == "__main__" :
     #             plt.ylabel("Number of solutions")
     #             plt.show()
 
-    # calculate the average accuracy for each method and plot both scales on the same bar chart
-    for team in teams:
-        for method in team.solvingMethods:
-            plt.figure()
-            plt.title("Team : "+team.name+" Method : "+method)
-            plt.bar(list_of_scales, [np.mean(data[scale][team.name][method]["accuracy_list"]) for scale in list_of_scales])
-            plt.xlabel("Scale")
-            plt.ylabel("Average accuracy")
-            plt.show()
+    # plot the results
 
-            
-    
+    ploting = importing_plots()
+    ploting.plot(data)
---
+++ Tests/plots/ploting.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def avg(l):
+    return sum(l)/len(l)
+
+
+def plot(data):
+    teams = ["Team 1", "Team 2"]
+    metrics = ['time', 'accuracy']
+    for metric in metrics:
+        for team in teams:
+            plot_metrics_per_method_per_scale(data, metric, team)
+
+    for team in teams:
+        plot_percentage_invalid_results(data, team)
+
+    for team in teams:
+        scatter_plot_accuracy_time(data, team)
+
+    plot_accuracy_per_team_per_scale(data)
+
+    plot_time_per_team_per_scale(data)
+
+
+# histograms of each algorithm for each team accuarcy mean
+def plot_metrics_per_method_per_scale(data, metric, team):
+    x = np.arange(len(data['low_scale'][team].keys()))
+    print(x, list(data['low_scale'][team].keys()))
+    plt.bar(x-0.1, [avg(j[metric+'_list'])
+            for j in [i for i in data['low_scale'][team].values()]], 0.2)
+    plt.bar(x+0.1, [avg(j[metric+'_list'])
+            for j in [i for i in data['large_scale'][team].values()]], 0.2)
+    plt.xticks(x, list(data['low_scale'][team].keys()))
+    plt.xlabel(team.capitalize()+" Algorithms", fontsize=15)
+    plt.ylabel(metric.capitalize() + " Mean", fontsize=15)
+    plt.title(metric.capitalize()+" of each algorithm for "+team)
+    plt.legend(["Low Scale", "Large Scale"])
+    plt.show()
+
+
+def plot_percentage_invalid_results(data, team):
+    x = np.arange(len(data['low_scale'][team].keys()))
+    plt.bar(x-0.1, [(len(j['time_list'])-j['nb_valid'])/len(j['accuracy_list'])
+            for j in [i for i in data['low_scale'][team].values()]], 0.2)
+    plt.bar(x+0.1, [(len(j['time_list'])-j['nb_valid'])/len(j['accuracy_list'])
+            for j in [i for i in data['large_scale'][team].values()]], 0.2)
+    plt.xticks(x, list(data['low_scale'][team].keys()))
+    plt.xlabel(team.capitalize()+" Algorithms", fontsize=15)
+    plt.ylabel("Percentage of invalid results")
+    plt.title("Percentage of invalid results for each algorithm "+team)
+    plt.legend(["Low Scale", "Large Scale"])
+    plt.show()
+
+
+def scatter_plot_accuracy_time(data, team):
+    average_accuracy = [avg(j['accuracy_list'])
+                        for j in [i for i in data['low_scale'][team].values()]]
+
+    # list of average time for each algorithm
+    average_time = [avg(j['time_list'])
+                    for j in [i for i in data['low_scale'][team].values()]]
+
+    plt.scatter(average_accuracy, average_time, c="blue")
+    plt.xlabel("Accuracy")
+    plt.ylabel("Time")
+    plt.title("Accuracy vs Time for "+team)
+    plt.show()
+
+
+def plot_accuracy_per_team_per_scale(data):
+    low_scale_time_team1 = [avg(j['time_list']) for j in [
+        i for i in data['low_scale']['Team 1'].values()]]
+    low_scale_time_team2 = [avg(j['time_list']) for j in [
+        i for i in data['low_scale']['Team 2'].values()]]
+    large_scale_time_team1 = [avg(j['time_list']) for j in [
+        i for i in data['large_scale']['Team 1'].values()]]
+    large_scale_time_team2 = [avg(j['time_list']) for j in [
+        i for i in data['large_scale']['Team 2'].values()]]
+    low_scale_avg_time_team1 = avg(low_scale_time_team1)
+    low_scale_avg_time_team2 = avg(low_scale_time_team2)
+    large_scale_avg_time_team1 = avg(large_scale_time_team1)
+    large_scale_avg_time_team2 = avg(large_scale_time_team2)
+
+    x = np.arange(2)
+    plt.bar(x-0.1, [low_scale_avg_time_team1, low_scale_avg_time_team2], 0.2)
+    plt.bar(x+0.1, [large_scale_avg_time_team1,
+            large_scale_avg_time_team2], 0.2)
+
+    plt.xticks(x, list(data['low_scale'].keys()))
+    plt.xlabel("Teams", fontsize=15)
+    plt.ylabel("Time")
+    plt.title("Time of each team")
+    plt.legend(["Low Scale", "Large Scale"])
+    plt.show()
+
+
+def plot_time_per_team_per_scale(data):
+    low_scale_Accuracy_team1 = [avg(j['accuracy_list']) for j in [
+        i for i in data['low_scale']['Team 1'].values()]]
+    low_scale_Accuracy_team2 = [avg(j['accuracy_list']) for j in [
+        i for i in data['low_scale']['Team 2'].values()]]
+    large_scale_Accuracy_team1 = [avg(j['accuracy_list']) for j in [
+        i for i in data['large_scale']['Team 1'].values()]]
+    large_scale_Accuracy_team2 = [avg(j['accuracy_list']) for j in [
+        i for i in data['large_scale']['Team 2'].values()]]
+
+    low_scale_avg_Accuracy_team1 = avg(low_scale_Accuracy_team1)
+    low_scale_avg_Accuracy_team2 = avg(low_scale_Accuracy_team2)
+    large_scale_avg_Accuracy_team1 = avg(large_scale_Accuracy_team1)
+    large_scale_avg_Accuracy_team2 = avg(large_scale_Accuracy_team2)
+    x = np.arange(2)
+    plt.bar(x-0.1, [low_scale_avg_Accuracy_team1,
+            low_scale_avg_Accuracy_team2], 0.2)
+    plt.bar(x+0.1, [large_scale_avg_Accuracy_team1,
+            large_scale_avg_Accuracy_team2], 0.2)
+
+    plt.xticks(x, list(data['low_scale'].keys()))
+    plt.xlabel("Teams", fontsize=15)
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy of each team")
+    plt.legend(["Low Scale", "Large Scale"])
+    plt.show()
